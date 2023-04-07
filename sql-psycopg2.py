8a87806ede04518253290f3b8ceddbe286a45a4a
Oran-123 <orandillon123@gmail.com>
2023-04-07 11:29:07 +0000

    update to many files

---
+++ sql-psycopg2.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+# connect to "chinook" db
+
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object 
+
+cursor = connection.cursor()
+
+# query 1 - select all records from the "Artist" table
+
+cursor.execute('SELECT * FROM "Artist"')
+
+#fetch
+
+results = cursor.fetchall()
+
+# fetch the result(single)
+# results = cursor.fetchone()
+
+#close hte connection
+
+connection.close()
+
+#print results 
+
+for result in results:
+    print(result)
---
+++ sql.crud.py
@@ -0,0 +1,55 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+#class based model
+
+class Programmer(base):
+    __tablename__="Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+#~creating records on programme table
+
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
+)
+#add each instance of our programmers
+session.add(ada_lovelace)
+
+# commit our session
+session.commit()
+
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
